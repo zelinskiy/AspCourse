381f2665c44514526de9b4f1953cc1ae0ac9e129
zelinskiy <zelinskiy1917@gmail.com>
2016-07-20 12:33:31 +0300

    Moved ChatScripts to separate file

---
+++ src/AspCourse/wwwroot/js/ChatScripts.js
@@ -0,0 +1,103 @@
+﻿
+function sendNewMessage(id) {
+    $.post({
+        url: "/Chat/AddNewMessage",
+        data: {
+            NewMessageText: $('#newMessageText').val(),
+            NewMessageTopicId: id,
+            NewMessagePictureUrl: $('#newMessagePictureUrl').val()
+        },
+        success: function (data) {
+            console.log(data);
+            if (data == "YOU ARE MUTED") {
+                alert("You are muted!");
+            }
+            window.location.reload();
+        },
+    });
+}
+
+function removeMessage(id) {
+    $.post({
+        url: "/Chat/RemoveMessage/?id=" + id,
+        success: function (data) {
+            console.log(data);
+            window.location.reload();
+        }
+    })
+}
+
+function replyToMessage(id) {
+    $('#newMessageText').val($('#newMessageText').val() + '>>' + id + '\n');
+}
+
+
+
+
+
+function uploadImage() {
+    var data = new FormData($("#uploadForm")[0]);
+
+    $.ajax({
+        url: "/File/AddFile",
+        type: "POST",
+        data: data,
+        contentType: false,
+        dataType: false,
+        processData: false,
+        success: function (result) {
+            $("#newMessagePictureUrl").val("/uploads/" + result);
+        }
+    });
+}
+
+
+function sendNewTopic() {
+    $.post({
+        url: "/Chat/AddNewTopic",
+        data: {
+            NewMessageText: $('#newTopicText').val(),
+            NewTopicTitle: $('#newTopicTitle').val(),
+            NewMessagePictureUrl: $('#newMessagePictureUrl').val()
+        },
+        success: function (data) {
+            window.location.reload();
+            console.log(data);
+        }
+    });
+}
+
+function removeTopic(id) {
+    $.post({
+        url: "/Chat/RemoveTopic/?id=" + id,
+        success: function (data) {
+            console.log(data);
+            window.location.reload();
+        }
+    })
+}
+
+
+function toggleTopicSticked(id) {
+    $.post({
+        url: "/Chat/ToggleTopicSticky/?id=" + id,
+        type: "POST",
+        success: function (data) {
+            console.log(data);
+            window.location.reload();
+        }
+    })
+}
+
+
+function toggleTopicClosed(id) {
+    $.post({
+        url: "/Chat/ToggleTopicClosed/?id=" + id,
+        type: "POST",
+        success: function (data) {
+            console.log(data);
+            window.location.reload();
+        }
+    })
+}
+
