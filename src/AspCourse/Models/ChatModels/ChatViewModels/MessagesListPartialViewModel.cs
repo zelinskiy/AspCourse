27311ed2b6afc188f51127a3b3af62f0b4784f0f
zelinskiy <zelinskiy1917@gmail.com>
2016-07-20 12:25:37 +0300

    MessagesList as partial view

---
+++ src/AspCourse/Models/ChatModels/ChatViewModels/MessagesListPartialViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspCourse.Models.ChatModels.ChatViewModels
+{
+    public class MessagesListPartialViewModel
+    {
+        public int NumOfMessages { get; set; }
+
+        public bool IsModer { get; set; }
+        
+        public List<Message> Messages { get; set; }
+    }
+}
